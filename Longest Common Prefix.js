7b586ea79454573451024e28af2a70e09ed69422
Fandix <fandix0925@gmail.com>
2020-04-22 17:22:43 +0800

    update Longest Common Prefix

---
+++ Longest Common Prefix.js
@@ -0,0 +1,30 @@
+longestCommonPrefix = (strs) => {
+
+    const inputAmount = strs.length;
+    let count = 0;
+    let output = "";
+   
+    //Step 1 : Check input unempty
+    if (strs.length === 0) 
+        return "";
+
+    //Step 3 : Calculate
+    for(let i=0;i<strs[0].length;i++)
+    {
+        count = 0;
+        for(let j=1;j<inputAmount;j++)
+        {
+            if(strs[0][i] === strs[j][i])
+            {
+                count++;
+            }
+        }
+        if(i === 0 && count !== (inputAmount-1))
+            return "";
+
+        if(count === (inputAmount-1))
+            output += strs[0][i];
+    }
+    return output;
+};
+
